fd35dcbef9791f3d66df62e97d9b339f781cee5a
lisa4930007 <lisalai@gapp.nthu.edu.tw>
2021-04-18 21:57:41 +0800

    base+home html

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def home_page_view(request):
-    return HttpResponse('Hello, World!')
+class HomePageView(TemplateView):
+	template_name = 'home.html'
