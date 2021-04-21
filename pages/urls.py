69c6ab0c94655e38b1bd1f1a997a06fea5890053
lisa4930007 <lisalai@gapp.nthu.edu.tw>
2021-04-21 22:17:56 +0800

    add blessing page

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, BlessingPageView
 
 urlpatterns = [
 	path('', HomePageView.as_view(), name='home'),
+	path('blessing/', BlessingPageView.as_view(), name='blessing'),
 ]
